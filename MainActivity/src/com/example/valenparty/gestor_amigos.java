17c67c907e8e89739441708cae810e294f21e3cf
Alvaro Vilanova <alvivi@gmail.com>
2013-01-12 13:02:51 +0100

    Añadido gestión de amigos

--- MainActivity/src/com/example/valenparty/gestor_amigos.java
+++ MainActivity/src/com/example/valenparty/gestor_amigos.java
@@ -1,9 +1,15 @@
 package com.example.valenparty;
 
+import java.util.ArrayList;
+
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+
 
 import android.os.Bundle;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,51 +18,71 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Data;
-import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 
-public class gestor_amigos extends SherlockActivity implements OnClickListener{
-	private Button mBtnContacts;
+public class gestor_amigos extends SherlockActivity{
+
 	private final int PICK = 1;
+	private ListView mContactList;
+	Myhelper BD;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gestor_amigos);
-		mBtnContacts = (Button) findViewById(R.id.xBtnContacts);
-		mBtnContacts.setOnClickListener(this);
+		
+		mContactList = (ListView) findViewById(R.id.listView1);
+		generarListaContactos();
+		registerForContextMenu(mContactList);
+
 	}
 
-	@TargetApi(5)
-	public void onClick(View v) {
-		// Opening Contacts Window as a Window
-		Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-		// calling OnActivityResult with intenet And Some conatct for Identifie
-		startActivityForResult(intent, PICK);
-	}
+	private void generarListaContactos() {
+      
+    	BD = new Myhelper(this, "DBUsuarios");
+    	SQLiteDatabase db = BD.getWritableDatabase();
+ 
+    	// Hacemos la consulta para recuperar nuestros datos
+    	String consulta = "select * from mytable";
+    	
+    	// Usamos un cursor para recorrer la consulta
+    	Cursor c = db.rawQuery(consulta,null);
+    	ArrayList<String> lista = new ArrayList<String>();
+    	while(c.moveToNext()){
+    		String num = c.getString(1);
+    		String nom = c.getString(2);
+    		lista.add(nom + " " + num); 
+    	}
+    	db.close(); 
+
+		 
+//       String strings[] = new String[100];
+//       for (int i = 0; i < strings.length; i++)
+//    	   strings[i] = new String("Elemento "+i);     
+   
+      
+        mContactList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lista));
+
+
+    }
 
 	@Override
 	public void onActivityResult(int reqCode, int resultCode, Intent data) {
 	  super.onActivityResult(reqCode, resultCode, data);
 	  if (resultCode == Activity.RESULT_OK){
-	     //Uri donde estan los datos de los contactos.
+	     //Uri del contacto que hemos elegido
 		 Uri contactData = data.getData();
+		 //Sacamos el id del contacto
 		 String id = contactData.getLastPathSegment();
-		 Log.d("Contactos",contactData.toString());
-		 Uri URI = ContactsContract.Data.CONTENT_URI;
-		 Log.d("Contactos",URI.toString());
-		 Log.d("Contactos","Paso 1");
-		 //abrimos la base de datos
-		 Myhelper BD = new Myhelper(this, "DBUsuarios");
-		 SQLiteDatabase db = BD.getWritableDatabase();
-		// db.execSQL("DROP TABLE IF EXISTS mytable");
-		// db.execSQL("CREATE TABLE mytable (numero TEXT PRIMARY KEY, nombre TEXT)");
 	   
 		 //solo quiero estos campos
 		 String[] projection = new String[]{
@@ -65,59 +91,91 @@ public class gestor_amigos extends SherlockActivity implements OnClickListener{
 		 };	
 		 
 		 String where = Phone.CONTACT_ID + "=?";
-		 //consulta en la que obtenemos los datos.
+		 //Usamos un cursor para recuperar los datos del contacto
+		 
+		 
 	 	 Cursor cursor = getContentResolver().query(Phone.CONTENT_URI, projection , where, new String[]{id}, null);
-	 	 Log.d("Contactos","Paso 2");
 	   	 cursor.moveToFirst();
 		
-	   	 //Buscamos la columna donde esta el nombre, obtenemos el nombre y lo imprimimos.
-	   	 //Buscamos la columna donde esta el numero, obtenemos el numero y lo imprimimos.
+	   	 //Recuperamos los datos del contacto y los insertamos en la base de datos
 	 
 	   	 int colIdx = cursor.getColumnIndex(Data.DISPLAY_NAME);
 	   	 String  nombre = cursor.getString(colIdx);
-		 Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show();
 		 
 		 colIdx = cursor.getColumnIndex(Phone.NUMBER);
 		 String numero = cursor.getString(colIdx);
-		 Toast.makeText(this, numero, Toast.LENGTH_SHORT).show();
 		 
-		 String insert = "INSERT INTO mytable (numero, nombre) " + "VALUES ('" + numero +"', '" + nombre +"')";
+		 //abrimos la base de datos
+		 BD = new Myhelper(this, "DBUsuarios");
+		 SQLiteDatabase db = BD.getWritableDatabase();
+//		 db.execSQL("DROP TABLE IF EXISTS mytable");
+//		 db.execSQL("CREATE TABLE mytable (numero TEXT PRIMARY KEY, nombre TEXT)");
+		 
+		 String insert = "INSERT INTO mytable (numero, nombre, latitud, longitud, estado) " + "VALUES ('" + numero +"', '" + nombre +"', '0', '0', 'POR_CONFIRMAR')";
 		 db.execSQL(insert);
-		 Log.d("Contactos","Paso 4");
-		 /*
-		 while(cursor.moveToNext()){
-	   		 
-	   		 colIdx = cursor.getColumnIndex(Data.DISPLAY_NAME);
-			 nombre = cursor.getString(colIdx);
-			 Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show();
-			 colIdx = cursor.getColumnIndex(Phone.NUMBER);
-			 numero = cursor.getString(colIdx);
-			 Toast.makeText(this, numero, Toast.LENGTH_SHORT).show();
-			 insert = "INSERT INTO mytable (numero, nombre) " + "VALUES ('" + numero +"', '" + nombre +"')";
-			 db.execSQL(insert);
-    	 }
-	   	 	*/		 
-		 
-		 
-		 
-		 // Hacemos la consulta para recuperar nuestros datos
-		 String consulta = "select * from mytable";
-			
-		 // Usamos un cursor para recorrer la consulta
-		 Cursor c = db.rawQuery(consulta,null);
-		
-		 Toast.makeText(this, "leyendo Base Datos", Toast.LENGTH_SHORT).show();
-		 
-		 while(c.moveToNext()){
-				String num = c.getString(0);
-				String nom = c.getString(1);
-				Toast.makeText(this, nom, Toast.LENGTH_SHORT).show();	
-				Toast.makeText(this, num, Toast.LENGTH_SHORT).show();  
-			}
-		 db.close();  	
-
-					
+		 //Cerramos la base de datos
+		 db.close();
+		 generarListaContactos();
 	  }		
 	}
+	
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	MenuInflater inflater = getSupportMenuInflater();
+    	inflater.inflate(R.menu.menu_amigos, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	int itemId = item.getItemId();
+		if (itemId == R.id.anyadir_amigo) {
+			// Opening Contacts Window as a Window
+			Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+			// calling OnActivityResult with intent And Some contact for Identifier
+			startActivityForResult(intent, PICK);
+			return true;			
+		}
+		return false;
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
 
+        android.view.MenuInflater inflater = getMenuInflater();
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+
+        menu.setHeaderTitle(mContactList.getAdapter().getItem(info.position).toString());
+
+        inflater.inflate(R.menu.menu_contextual_amigos, menu);
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+
+
+        AdapterContextMenuInfo info =
+            (AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.Contextual1:
+                consultarPosicion(info.position);
+                return true;
+            case R.id.Contextual2:
+                llamadaTelefonica(info.position);
+                return true;
+            case R.id.Contextual3:
+                borrarAmigo(info.position);
+                return true;
+            default:
+                return super.onContextItemSelected((android.view.MenuItem) item);
+        }
+    }
+    private void consultarPosicion(int id){
+    	
+    }
+    private void llamadaTelefonica(int id){
+    	
+    }
+    private void borrarAmigo(int id){
+    	
+    }
 }
